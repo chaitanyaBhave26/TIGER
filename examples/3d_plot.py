c3614b495c3d57872111ee937abcc30951758b6d
chaitanyaBhave26 <chaitanya.bhave@ufl.edu>
2022-03-30 14:01:09 -0400

    Added documentation for 3D plot code.

--- examples/3d_plot.py
+++ examples/3d_plot.py
@@ -10,32 +10,42 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 from mpl_toolkits.mplot3d import Axes3D
 
+#EXODUS FILE FOR RENDERING
+#ANY CHARACTER(S) CAN BE PLACED IN PLACE OF THE *, EG. 2D/grain_growth_2D_graintracker_out.e.1921.0000 or 2D/grain_growth_2D_graintracker_out.e-s001
 filenames = '3D/grain_growth_3D_out.e*'                             #Star represents all files following this template
 
-##Open multi exodus reader
+#READ EXODUS FILE SERIES WITH MultiExodusReader
 MF = MultiExodusReader(filenames)
 
-##List of all times for which exodus is read
+#GET A LIST OF SIMULATION TIME POINTS
 times = MF.global_times
 
+#GENERATE FIGURE WINDOW AND SUBPLOT AXIS
 fig = plt.figure()
 ax = fig.add_subplot('111',projection='3d')
 
-
+#GET X,Y,Z AND C (UNIQUE GRAINS VALUES) AT CURRENT TIME STEP
 x,y,z,c = MF.get_data_at_time('unique_grains',MF.global_times[-1])               #Read coordinates and variable value --> Will be parallelized in future
 
+#CREATE A COLORMAP OBJECT (cw) THAT CONVERTS VARIABLE VALUE c INTO A RGBA VALUE
 c_min = np.amin(c)
 c_max = np.amax(c)
-
+#USING COLORMAP HSV. MATPLOTLIB HAS A LIST OF COLORMAPS YOU CAN USE, OR YOU CAN EVEN GENERATE YOUR OWN CUSTOM MAP
 cw = matplotlib.cm.ScalarMappable(cmap=matplotlib.cm.hsv)
 cw.set_array([c_min,c_max])
 
+#CONVERT VARIABLE VALUES TO RGBA VALUES
 C = cw.to_rgba(c)
 
+#LIST OF SURFACES, AND THEIR CORRESPONDING COLORS
 surfaces = []
 colors = []
 
+#GENERATE CORNER POINT COORDINATES FOR THE QUAD8 MESH POLYGONS
 coords = np.asarray([ np.asarray([x_val,y_val,z_val]).T for (x_val,y_val,z_val) in zip(x,y,z) ])
+
+#GENERATE THE 6 SIDES FOR EACH POLYGON, AND ASSIGN THE CELL VALUE OF THE POLYGON TO THE sides
+#THIS CODE IS VERY UNOPTIMIZED, AND WILL BE IMPROVED IN THE FUTURE
 for (i,side) in enumerate(coords):
     sides = [ [side[0],side[1],side[2],side[3]],
                      [side[4],side[5],side[6],side[7]],
@@ -47,16 +57,25 @@ for (i,side) in enumerate(coords):
     surfaces+=sides
     colors+=c_temp
 
+#CREATE A Poly3DCollection FROM OUR SURFACES
 P = Poly3DCollection(surfaces, facecolors=colors,   alpha=1.0)
+
+#PLOT THE POLY3DCOLLECTION ON OUR AXIS
 collection = ax.add_collection3d(P)
 
+#FIGURE FORMATTING SETTINGS
 ax.set_xlim([0,1000])                                                                   #You can use x and y arrays for setting this, but usually it is easier to manually set
 ax.set_ylim([0,1000])
 ax.set_zlim([0,1000])
 
-
+#CREATE COLORBAR FROM OUR COLORBAR OBJECT CW
 fig.colorbar(cw,label="Unique Grains")
+
+#TIGHT LAYOUT TO AUTOMATICALLY ADJUST BORDERS AND PADDING FOR BEST LOOKING IMAGE
 fig.tight_layout()
+
+#SAVE FIGURE IN DESIRED DPI AND TRANSPARENCY
 fig.savefig('3D/3d_render.png',dpi=500,transparent=True )             #Remember to create the folder pyrender to store images in!!
 
+#USE PLT.SHOW FOR AN INTERACTIVE 3D DISPLAY OF IMAGE. THIS CAN BE A BIT SLOW TO MANIPULATE DEPENDING ON EXODUS FILE SIZE
 plt.show()
