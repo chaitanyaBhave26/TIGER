50d72db504e6a72f8a76033d234f4e25a7f8ea88
chaitanyaBhave26 <chaitanya.bhave@ufl.edu>
2022-03-30 08:30:02 -0400

    Fixed extension for gif generator filename

--- examples/image_to_gif.py
+++ examples/image_to_gif.py
@@ -5,7 +5,7 @@ import os
 #DEFINE BASE NAME FOR FILES
 file_names = 'temp/1d_exodus_line_plot_'
 #NAME OF VIDEO FILE TO GENERATE
-video_file_name = '2D/2D_render.avi'
+gif_file_name = '2D/2D_render.gif'
 #PARAMETERS FOR VIDEO
 n_frames = 200
 fps = 20
@@ -17,4 +17,4 @@ for i in range(n_frames):
 #EACH FRAME LASTS 1000/FPS MILLISECONDS
 frame_duration = 1000/fps
 #SAVE PIL.IMAGE SEQUENCE INTO A GIF --> TURN ON OPTIMIZE FOR SMALLER FILE SIZE
-frames[0].save('2D/2d_render.gif',save_all=True,optimize=False,append_images=frames[1:],loop=0,duration=frame_duration)
+frames[0].save('gif_file_name',save_all=True,optimize=False,append_images=frames[1:],loop=0,duration=frame_duration)
